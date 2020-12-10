cbd727720626fe07f265f26252ea7ee88a887e61
JOKOJOKO405 <biz.kj405@gmail.com>
2020-12-10 11:05:02 +0900

    Add ますは普通に入力

---
+++ src/components/CounterMouse.vue
@@ -0,0 +1,47 @@
+<template>
+  <div @mousemove="updateMouse">
+    <div>
+      count is {{ count }}, count * {{ times }} is {{ multipleResult }}
+      <button @click="incrementCount">+</button>
+    </div>
+    <p>Mouse is at {{ mousePos.x}} and {{ mousePos.y }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { 
+    times: {
+      type: Number,
+      default: 2
+    }
+  },
+  data() {
+    return {
+      mousePos: { x: 0, y: 0 },
+      count: 0,
+    }
+  },
+  computed: { 
+    multipleResult(){
+      return this.count * this.times
+    }
+  },
+  watch:{
+    count(){
+      setTimeout(()=>{
+        alert('3秒後')
+      }, 3000)
+    }
+  },
+  methods: {
+    incrementCount(){
+      this.count ++
+    },
+    updateMouse(e){
+      this.mousePos.x = e.clientX
+      this.mousePos.y = e.clientY
+    }
+  }
+}
+</script>
